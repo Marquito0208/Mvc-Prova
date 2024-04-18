a77fc2909ab6f504efcaf3883ae8719ab51f33c3
marco marco <marcoaraujo0w@gmail.com>
2024-04-18 01:48:16 -0300

    Add project files.

---
+++ Mvc-Prova/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mvc_Prova.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Mvc-Prova/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mvc_Prova.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string UserPassword { get; set; } = string.Empty;
+        public string UserPhone { get; set; } = string.Empty;
+
+    }
+}
